88b9ea621e04ced761e14442e83ded5cc491ea31
Bangerok <kuznetsov.v.a.work@gmail.com>
2018-10-17 16:32:44 +0300

    [1.2.5] Добавил обработку ошибок и логгирование.

--- src/main/java/ru/kuznetsov/service/Application.java
+++ src/main/java/ru/kuznetsov/service/Application.java
@@ -1,27 +1,28 @@
 package ru.kuznetsov.service;
 
 import ru.kuznetsov.service.CBRClasses.*;
-
-import javax.xml.ws.ProtocolException;
+import java.util.logging.*;
 
 public class Application {
+    private static Logger log = Logger.getLogger(Application.class.getName());
+
     public static void main(String[] args){
         try {
             CreditOrgInfo_Service creditOrgInfo = new CreditOrgInfo_Service();
             CreditOrgInfoSoap soap = creditOrgInfo.getCreditOrgInfoSoap();
+            log.log(Level.INFO, "CreditOrgInfoSoap initialization was successful.");
 
             double internalCode = soap.bicToIntCode("040173725");
+            log.log(Level.INFO, "bicToIntCode returned internal code - " + Double.toString(internalCode));
 
             ArrayOfDouble internalCodes = new ArrayOfDouble();
             internalCodes.getDoubles().add(internalCode);
 
             CreditInfoByIntCodeExXMLResponse.CreditInfoByIntCodeExXMLResult info;
             info = soap.creditInfoByIntCodeExXML(internalCodes);
-
-            System.out.println("Request success!" + info.getContent().toString());
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-            System.out.println("Request error!");
+            log.log(Level.INFO, "creditInfoByIntCodeExXML returned data - " + info.getContent().toString());
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Exception - " + e.getMessage());
         }
     }
 }
