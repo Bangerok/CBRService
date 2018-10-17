62a652915114b87ed12107733fe60a45906ae688
Bangerok <kuznetsov.v.a.work@gmail.com>
2018-10-17 14:28:18 +0300

    [1.2.0] Добавил при разбирании WSDL binding файл xsd. Теперь нужный мне метод возвращает ответ с данными.

--- src/main/java/ru/kuznetsov/service/Application.java
+++ src/main/java/ru/kuznetsov/service/Application.java
@@ -2,16 +2,26 @@ package ru.kuznetsov.service;
 
 import ru.kuznetsov.service.CBRClasses.*;
 
+import javax.xml.ws.ProtocolException;
+
 public class Application {
     public static void main(String[] args){
-        CreditOrgInfo creditOrgInfo = new CreditOrgInfo();
-        CreditOrgInfoSoap soap = creditOrgInfo.getCreditOrgInfoSoap();
+        try {
+            CreditOrgInfo_Service creditOrgInfo = new CreditOrgInfo_Service();
+            CreditOrgInfoSoap soap = creditOrgInfo.getCreditOrgInfoSoap();
 
-        double internalCode = soap.bicToIntCode("040173725");
+            double internalCode = soap.bicToIntCode("040173725");
 
-        ArrayOfDouble internalCodes = new ArrayOfDouble();
-        internalCodes.getDoubles().add(internalCode);
+            ArrayOfDouble internalCodes = new ArrayOfDouble();
+            internalCodes.getDoubles().add(internalCode);
 
-        CreditInfoByIntCodeExXMLResponse.CreditInfoByIntCodeExXMLResult info = soap.creditInfoByIntCodeExXML(internalCodes);
+            CreditInfoByIntCodeExXMLResponse.CreditInfoByIntCodeExXMLResult info;
+            info = soap.creditInfoByIntCodeExXML(internalCodes);
+
+            System.out.println("Request success!" + info.getContent().toString());
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+            System.out.println("Request error!");
+        }
     }
 }
